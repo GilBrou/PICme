c7729919d59d1b57caf5bc38a60468c9c208e5e2
Gillian Brousse <gillian.brousse@neuf.fr>
2022-07-09 03:24:46 +0200

    last refacto

--- Js/init.js
+++ Js/init.js
@@ -1,7 +1,7 @@
 //Get Json Data
 const myFetch = async () =>{ 
 
-  return await fetch('https://raw.githubusercontent.com/GilBrou/PICme/main/FishEyeData.json')
+  return await fetch('https://raw.githubusercontent.com/GilBrou/PICme/master/FishEyeData.json')
   .then(function(resp){return resp.json();})
   .then(function(data){return data})
   .catch(function(error) {console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);});
